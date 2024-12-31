0d5dcb0bc67cc8902505c4c2817447d1d8b1828d
iubieta <ikerp.ubieta@gmail.com>
2024-12-31 17:41:57 +0100

    Solucionado problema con clangd

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,8 +17,14 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require('lspconfig')
+			
 			lspconfig.lua_ls.setup({})
-			lspconfig.clangd.setup({})
+			lspconfig.clangd.setup({	
+				cmd = { "clangd" },
+				filetypes = { "c", "cpp", "objc", "objcpp" },
+				root_dir = require('lspconfig.util').root_pattern("compile_commands.json", ".git"),
+				single_file_support = true
+			})
 
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
