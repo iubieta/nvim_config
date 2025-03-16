dbd75111ffe75e8e337c9a22732943095c144cd8
iubieta <ikerp.ubieta@gmail.com>
2025-03-16 17:28:05 +0100

    Ctrl-W y Ctrl-C

--- lua/vim-keymaps.lua
+++ lua/vim-keymaps.lua
@@ -2,9 +2,14 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- Escape
+vim.api.nvim_set_keymap('i', '<C-c>', '<Esc>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-c>', '<Esc>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-c>', '<Esc>', { noremap = true, silent = true })
+
 -- Movimiento
-vim.api.nvim_set_keymap('n', 'J', '<C-d>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'K', '<C-u>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'J', ':15j<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'K', ':15k<CR>', { noremap = true, silent = true })
 
 -- Movimiento en modo insercion con ctrl
 local keys = { 'w', 'b', 'e', '0', '$', 'gg', 'G'}
@@ -25,7 +30,7 @@ vim.keymap.set("n", "<leader>w", ':w<CR>', { desc = "Guardar" })
 vim.keymap.set("n", "<leader>wa", ':wa<CR>', { desc = "Guardar todo" })
 
 -- Cerrar archivo
-vim.api.nvim_set_keymap('n', '<C-d>', ':q<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-w>', ':q<CR>', { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>q", ':q<CR>', { desc = "Cerrar" })
 vim.keymap.set("n", "<leader>qa", ':qa<CR>', { desc = "Cerrar todo" })
 
